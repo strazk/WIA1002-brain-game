85a9ce8a03aa410c8cb3f003a5d384ae6a291106
Admin <Admin@LAPTOP-E4FV00B4>
2019-04-23 14:18:07 +0700

    brain

--- Brain game/src/brain/game/BrainGame.java
+++ Brain game/src/brain/game/BrainGame.java
@@ -15,7 +15,7 @@ public class BrainGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //testing
+       //my name is siti 
     }
     
 }
